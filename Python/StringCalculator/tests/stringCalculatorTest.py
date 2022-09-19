cbf4c4e5244a69a2b8286629025e243b83f6bd6d
jeeva <jeeva.ramu@codurance.com>
2022-09-19 16:09:40 +0100

    refactored the test as parameterised

--- Python/StringCalculator/tests/stringCalculatorTest.py
+++ Python/StringCalculator/tests/stringCalculatorTest.py
@@ -1,11 +1,18 @@
-from unittest import TestCase
+import unittest
+from parameterized import parameterized
 from src.stringCalculator import StringCalculator
 
 
-class StringCalculatorTest(TestCase):
-    def test_should_return_zero_for_empty_string(self):
+class StringCalculatorTest(unittest.TestCase):
+
+    @parameterized.expand([
+        [""],
+        [" "],
+        ["  "],
+    ])
+    def test_should_return_zero_for_empty_string(self, number):
         calculator = StringCalculator()
-        actual = calculator.Add(" ")
+        actual = calculator.Add(number)
         self.assertEqual(actual, 0)
 
     def test_should_return_non_zero_for_number_string(self):
