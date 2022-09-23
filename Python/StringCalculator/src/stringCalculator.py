c30701361091391cd37096c00a25dd8158e71c3d
jeeva <jeeva.ramu@codurance.com>
2022-09-23 09:25:47 +0100

    Refactored Add Method.

--- Python/StringCalculator/src/stringCalculator.py
+++ Python/StringCalculator/src/stringCalculator.py
@@ -5,18 +5,15 @@ class StringCalculator:
     def Add(self, numbers: str) -> int:
         if self.__IsEmptyString(numbers):
             return 0
+        elif len(numbers.strip()) == 1:
+            return int(numbers)
         else:
             numbers = self.__GetNumberCollection(numbers)
             self.__CheckNegativeNumbers(numbers)
-            sum_of_numbers = sum(numbers)
-            return sum_of_numbers
+            return sum(numbers)
 
     def __GetNumberCollection(self, numbers: str) -> list:
         collection = list([])
-        if len(numbers) == 1:
-            collection.extend(numbers)
-            return self.__MapToIntegerCollection(collection)
-
         for separator in self.__separators:
             if numbers.__contains__(separator):
                 collection = numbers.split(separator)
